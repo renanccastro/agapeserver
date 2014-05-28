a0d2172cdfe2133a38d2efbd08f691ff9b39f82b
Renan Castro <renanccastro@gmail.com>
2014-05-28 14:04:58 -0300

    Primeiro arquivo de teste do servidor

---
+++ serverTest.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/agape');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  var kittySchema = mongoose.Schema({
+    name: String
+	});
+  kittySchema.methods.speak = function () {
+  var greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name"
+  console.log(greeting);
+	};
+  var Kitten = mongoose.model('Kitten', kittySchema);
+  Kitten.find(function (err, kittens) {
+  if (err) return console.error(err);
+  console.log(kittens)
+	})
+  var silence = new Kitten({ name: 'Silence' })
+	console.log(silence.name) // 'Silence'
+	var fluffy = new Kitten({ name: 'fluffy' });
+	fluffy.speak() // "Meow name is fluffy"
+	fluffy.save(function (err, fluffy) {
+  if (err) return console.error(err);
+  fluffy.speak();
+	});
+});
