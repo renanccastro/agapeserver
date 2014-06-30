5c4fb0fe927a6e1139055865b67843110f5499a8
Renan Castro <renanccastro@gmail.com>
2014-06-30 09:53:14 -0300

    MongoDB teste

--- web.js
+++ web.js
@@ -2,11 +2,26 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
+});
+
+app.get('/teste', function(req, res){
+	mongo.Db.connect(mongoUri, function (err, db) {
+	  db.collection('users', function(er, collection) {
+	    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+	    });
+	  });
+	});
 });
 
 var port = Number(process.env.PORT || 5000);
