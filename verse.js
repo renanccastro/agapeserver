307eeff70fc485aacc32a3d115c15969a5d88e40
Brenda Oliveira Ramires <brenda.o.ramires@gmail.com>
2014-07-02 16:56:44 -0300

    Brenda mexendo com a 'carteira de versos'

---
+++ verse.js
@@ -0,0 +1,20 @@
+var mongodb = require('./mongo_db.js');
+
+module.exports.addVerse = function(request, res){
+
+mongodb.connect( function (err, db) {
+
+		var verse = request; //QUAL CAMPO PEGO DO REQUEST?
+		
+		//insere o verso no banco
+   		db.collection('verses').insert(verse, function(err, records) {
+			if (err)
+				throw err;
+			console.log("New verse added as "+records[0]._id);
+
+		//adiciona o verso na carteira do usuario
+		db.collection('users').update({ _.id: records[0].author}, {$set: {verses.unshift(records[0]._id)}});
+		});
+
+
+});
