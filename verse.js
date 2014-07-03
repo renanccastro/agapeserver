fc0375f3944270566cf1c08939f8dcbfb15fdd74
Brenda Oliveira Ramires <brenda.o.ramires@gmail.com>
2014-07-03 15:33:52 -0300

    Adicionando post do verse....

--- verse.js
+++ verse.js
@@ -2,20 +2,20 @@ var mongodb = require('./mongo_db.js');
 
 module.exports.addVerse = function(request, res){
 
-mongodb.connect( function (err, db) {
+    mongodb.connect( function (err, db) {
 
-		var verse = request.body;
+		    var verse = request.body;
     
-		//Inserts verse into the collection
-   	db.collection('verses').insert(verse, function(err, records) {
-		  if (err)
-			  throw err;
+		    //Inserts verse into the collection
+   	    db.collection('verses').insert(verse, function(err, records) {
+		        if (err)
+			          throw err;
 		  
-      console.log("New verse added as "+records[0]._id);
+            console.log("New verse added as "+records[0]._id);
 
-		  //Insert verse into the user's verses
-		  db.collection('users').update( { _id: records[0].author}, {$push: { verses: records[0] }});
-		
-   	});
-
+		      //Insert verse into the user's verses
+		      db.collection('users').update( { _id: records[0].author}, {$push: { verses: records[0] }});
+      
+   	    });
+  });
 });
