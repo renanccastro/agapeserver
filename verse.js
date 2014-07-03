2031eb8858771c2aec84907272e76ae1ea24638c
Brenda Oliveira Ramires <brenda.o.ramires@gmail.com>
2014-07-03 11:04:56 -0300

    Atualizando 'carteira de versos'

--- verse.js
+++ verse.js
@@ -4,19 +4,18 @@ module.exports.addVerse = function(request, res){
 
 mongodb.connect( function (err, db) {
 
-		var verse = request; //QUAL CAMPO PEGO DO REQUEST?
+		var verse = request.body;
+    
+		//Inserts verse into the collection
+   	db.collection('verses').insert(verse, function(err, records) {
+		  if (err)
+			  throw err;
+		  
+      console.log("New verse added as "+records[0]._id);
+
+		  //Insert verse into the user's verses
+		  db.collection('users').update( { _id: records[0].author}, {$push: { verses: records[0] }});
 		
-		//insere o verso no banco
-   		db.collection('verses').insert(verse, function(err, records) {
-			if (err)
-				throw err;
-			console.log("New verse added as "+records[0]._id);
-
-		//adiciona o verso na carteira do usuario
-		db.collection('users').update({ _.id: records[0].author}, {$set: {verses.unshift(records[0]._id)}});
-		//exemplo:
-		//db.users.update({_id : "472679979502123"}, {$push : { verses: "1234" }} )
-		});
-
+   	});
 
 });
