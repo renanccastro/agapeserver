e4dbad67c5c32d4b0e729d95337003a5bd29e5bb
Renan Castro <renanccastro@gmail.com>
2014-11-05 15:41:24 -0200

    Arrumando update on disconnect

--- web.js
+++ web.js
@@ -179,9 +179,26 @@ io.sockets.on('connection', function(socket) {
 			});
 			var userIds = [];
 			io.sockets.clients(actual_room).forEach(function(s) {
-				userIds.push(s.username);
+				if (s.username) {
+					userIds.push({
+						"userId": s.username,
+						"online": "yes"
+					});
+					console.log("Username: " + s.username + "na sala: " + room);
+				}
+
 			});
-			io.sockets.emit('updateusers', actual_room, userIds);
+			redis_client.lrange(actual_room, 0, -1, function(error, items) {
+				if (error)
+					console.log("deu erro na hora de pegar a lista no redis");
+				items.forEach(function(offline_user) {
+					userIds.push({
+						"userId": offline_user,
+						"online": "no"
+					});
+				});
+			});
+			io.sockets.to(actual_room).emit('updateusers', actual_room, userIds);
 		}
 	});
 
